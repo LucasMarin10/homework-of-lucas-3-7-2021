143d24d67babe03fe710cb7713fb389ca5b8450d
LucasMarin10 <84347671+LucasMarin10@users.noreply.github.com>
2021-07-03 09:48:41 -0300

    Add files via upload

---
+++ Lucas_210703.py
@@ -0,0 +1,35 @@
+print("question 2")
+fruits=["pear","strawberry","orange","grape","apple"]
+print("question 3")
+print(fruits)
+print("question 4")
+print(fruits[:3])
+print("question 5")
+print(fruits[2:])
+print("question 6")
+print(fruits[1:4])
+print("question 7")
+print(fruits[::2])
+print("question 8")
+print(fruits[::-2])
+print("question 9")
+for h in fruits[::2]:
+    print(h)
+print("question 10")
+tupe=(10,200,300)
+print("question 11")
+for oh in tupe:
+    print(oh)
+print("question 12")
+asab=["apple","Apple","APPLE","banana"]
+print("question 13")
+for cool in asab:
+    print(cool)
+print("question 14")
+for opsl in asab:
+    if opsl == "apple":
+        print(opsl)
+print("question 15")
+for twbe in asab:
+    if twbe.upper() == "APPLE":
+        print(twbe.lower())
